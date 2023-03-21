b870a0df3ba5bc58ed751e51dd4a598a7338003f
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-21 16:12:10 -0500

    put correct files under features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./features/Header";
+import Home from "./features/Home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
