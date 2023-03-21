965f279eb00f0b7eb2f0a6f60b34de60d1cd9c88
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-21 15:14:42 -0500

    added subRedditSlice added and exported reducers

--- src/store/redditSlice.js
+++ src/store/redditSlice.js
@@ -72,3 +72,51 @@ export const {
 } = redditSlice.actions;
 
 export default redditSlice.reducer;
+
+// This is a Redux Thunk that gets posts from a subreddit.
+export const fetchPosts = (subreddit) => async (dispatch) => {
+  try {
+    dispatch(startGetPosts());
+    const posts = await getSubredditPosts(subreddit);
+
+    // We are adding showingComments and comments as additional fields to handle showing them when the user wants to. We need to do this because we need to call another API endpoint to get the comments for each post.
+    const postsWithMetadata = posts.map((post) => ({
+      ...post,
+      showingComments: false,
+      comments: [],
+      loadingComments: false,
+      errorComments: false,
+    }));
+    dispatch(getPostsSuccess(postsWithMetadata));
+  } catch (error) {
+    dispatch(getPostsFailed());
+  }
+};
+
+export const fetchComments = (index, permalink) => async (dispatch) => {
+  try {
+    dispatch(startGetComments(index));
+    const comments = await getPostComments(permalink);
+    dispatch(getCommentsSuccess({ index, comments }));
+  } catch (error) {
+    dispatch(getCommentsFailed(index));
+  }
+};
+
+const selectPosts = (state) => state.reddit.posts;
+const selectSearchTerm = (state) => state.reddit.searchTerm;
+export const selectSelectedSubreddit = (state) =>
+  state.reddit.selectedSubreddit;
+
+export const selectFilteredPosts = createSelector(
+  [selectPosts, selectSearchTerm],
+  (posts, searchTerm) => {
+    if (searchTerm !== "") {
+      return posts.filter((post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    return posts;
+  }
+);
