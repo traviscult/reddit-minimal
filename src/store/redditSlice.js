09a67b07bdcefc3fb1136628e7341b9bd6e2d17d
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-21 15:13:27 -0500

    added and exported reducers to redditSlice

--- src/store/redditSlice.js
+++ src/store/redditSlice.js
@@ -1,2 +1,74 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { getSubredditPosts, getPostComments } from "../api/reddit";
+
+const initialState = {
+  posts: [],
+  searchTerm: "",
+  selectedSubreddit: "/r/programmingmemes/",
+};
+
+const redditSlice = createSlice({
+  name: "redditPosts",
+  initialState,
+  reducers: {
+    setPosts(state, action) {
+      state.posts = action.payload;
+    },
+    startGetPosts(state) {
+      state.isLoading = true;
+      state.error = false;
+    },
+    getPostsSuccess(state, action) {
+      state.isLoading = false;
+      state.posts = action.payload;
+    },
+    getPostsFailed(state) {
+      state.isLoading = false;
+      state.error = true;
+    },
+    setSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    setSelectedSubreddit(state, action) {
+      state.selectedSubreddit = action.payload;
+      state.searchTerm = "";
+    },
+    toggleShowingComments(state, action) {
+      state.posts[action.payload].showingComments =
+        !state.posts[action.payload].showingComments;
+    },
+    startGetComments(state, action) {
+      // If hiding comments, don't fetch the comments.
+      state.posts[action.payload].showingComments =
+        !state.posts[action.payload].showingComments;
+      if (!state.posts[action.payload].showingComments) {
+        return;
+      }
+      state.posts[action.payload].loadingComments = true;
+      state.posts[action.payload].error = false;
+    },
+    getCommentsSuccess(state, action) {
+      state.posts[action.payload.index].loadingComments = false;
+      state.posts[action.payload.index].comments = action.payload.comments;
+    },
+    getCommentsFailed(state, action) {
+      state.posts[action.payload].loadingComments = false;
+      state.posts[action.payload].error = true;
+    },
+  },
+});
+
+export const {
+  setPosts,
+  getPostsFailed,
+  getPostsSuccess,
+  startGetPosts,
+  setSearchTerm,
+  setSelectedSubreddit,
+  toggleShowingComments,
+  getCommentsFailed,
+  getCommentsSuccess,
+  startGetComments,
+} = redditSlice.actions;
+
+export default redditSlice.reducer;
